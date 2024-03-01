b58749230509e2e5a7a4991efdec4442e7df21cf
Akatsuki Tushan <pu@mtu.edu>
2024-03-01 13:46:36 +0800

    Create prototype_parentheses_detection.py
    Practice from the book *Algorithms 4th Ed*, Ch.1.

---
+++ prototype_parentheses_detection.py
@@ -0,0 +1,38 @@
+def parenthesesCheck(str):
+    l = len(str)
+    if l == 1 or l == 0:
+        return False
+
+    check = []
+    check.append(str[0])
+    ic = 1
+
+    for i in range(1, l):
+        if str[i] == ")" and check[-1] == "(":
+            check.pop()
+            ic -= 1
+        elif str[i] == "]" and check[-1] == "[":
+            check.pop()
+            ic -= 1
+        elif str[i] == "}" and check[-1] == "{":
+            check.pop()
+            ic -= 1
+        else:
+            check.append(str[i])
+            ic += 1
+
+    if ic > 0:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    str1 = ["[", "(", ")", "]", "{", "}", "{", "[", "(", ")", "]", "(", ")", "}"]
+    str2 = ["[", "(", "]", ")"]
+
+    flag1 = parenthesesCheck(str1)
+    flag2 = parenthesesCheck(str2)
+
+    print(f"flag1 = {flag1}\n")
+    print(f"flag2 = {flag2}\n")
