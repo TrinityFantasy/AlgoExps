99f64b97b12fdd393314440040d57c54fd12841c
Akatsuki Tushan <pu@mtu.edu>
2024-03-01 13:39:00 +0800

    Create prototype_binary_search.py
    Practice from the book *Algorithms Illuminated Vol.1*, Ch.1.

---
+++ prototype_binary_search.py
@@ -0,0 +1,40 @@
+import numpy as np
+import time as t
+
+def __read(fname):
+    data = np.loadtxt(fname)
+    ldata = len(data)
+
+    return data,ldata
+
+def __binSearch(key,arr,larr):
+    lo = 0
+    hi = larr-1
+    mid = int((lo+hi)/2)
+
+    while lo<hi:
+        if arr[mid]>key:
+            hi=mid-1
+            mid=int((lo+hi)/2) 
+        elif arr[mid]<key:
+            lo=mid+1
+            mid=int((lo+hi)/2)
+        else:
+            return mid
+        
+if __name__ == "__main__":
+    fpath = "/home/akatsuki/Algorithms/BinarySearch/"
+    fname = "largeText.txt"
+    finput = fpath+fname
+
+    data,ldata = __read(fname)
+    #data,ldata = __read(finput)
+
+    data = np.sort(data,kind='mergesort')
+
+    key = 1201
+    s = t.time()
+    res = __binSearch(key,data,ldata)
+    e = t.time()
+    cost = e - s
+    print(f'The 1st index of {key} is: {res}. Time cost is: {cost} Sec.\n')
